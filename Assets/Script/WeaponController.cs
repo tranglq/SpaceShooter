916154f2f198286a5db67208f2471b4a01bb0900
tranglq <lequynhtrang120296@gmail.com>
2017-09-08 17:23:55 +0700

    Setting Weapon

---
+++ Assets/Script/WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+
+    private AudioSource audioSource;
+	
+	void Start () {
+        InvokeRepeating("Fire", delay, fireRate);
+	}
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        GetComponent<AudioSource>().Play();
+    }
+	
+}
